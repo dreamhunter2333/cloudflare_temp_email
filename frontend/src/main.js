4cae64b537fe03666c3865f47b940fd72f8e0607
Dream Hunter <dreamhunter2333@gmail.com>
2023-09-07 14:11:33 +0800

    feat: add i18n (#14)
    * feat: add i18n
    
    * feat: update readme

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createI18n } from 'vue-i18n'
 
+const i18n = createI18n({
+    legacy: false, // you must set `false`, to use Composition API
+    locale: 'zh', // set locale
+    fallbackLocale: 'en', // set fallback locale
+    'en': {
+        messages: {}
+    },
+    'zhCN': {
+        messages: {}
+    }
+})
 const app = createApp(App)
+app.use(i18n)
 app.mount('#app')
