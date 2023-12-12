25d654ff5a26d91a4009ba2f4649ab54bd160e79
dreamhunter2333 <dreamhunter2333@gmail.com>
2023-12-12 16:52:03 +0800

    feat: admin query emails

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,24 @@ export default defineConfig({
   },
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Temp Email',
+        short_name: 'Temp Email',
+        description: 'Temp Email - Temporary Email',
+        icons: [
+          {
+            src: '/logo.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
