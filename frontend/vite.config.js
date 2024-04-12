b99ca1fa1c7e8b6771d3776ff3d817fe942aed6b
dreamhunter2333 <dreamhunter2333@gmail.com>
2024-04-12 23:04:15 +0800

    feat: add Tag Build CI

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ import topLevelAwait from "vite-plugin-top-level-await";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: '../dist',
+    outDir: './dist',
   },
   plugins: [
     vue(),
