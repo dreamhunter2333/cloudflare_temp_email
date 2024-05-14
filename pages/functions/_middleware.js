441f5c3476ff9c261103bad77088b5ca690ff70b
dreamhunter2333 <dreamhunter2333@gmail.com>
2024-05-14 12:42:17 +0800

    feat: add page functions proxy to make response faster

---
+++ pages/functions/_middleware.js
@@ -0,0 +1,9 @@
+const API_PATHS = ["/api/", "/open_api/", "/user_api/", "/admin/"]
+
+export async function onRequest(context) {
+    const reqPath = new URL(context.request.url).pathname;
+    if (API_PATHS.map(path => reqPath.startsWith(path)).some(Boolean)) {
+        return context.env.BACKEND.fetch(context.request);
+    }
+    return await context.next();
+}
