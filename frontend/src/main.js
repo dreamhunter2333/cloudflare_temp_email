525f5e2dcef0a20e5ad5aa3da8cf9bd3d657e3ac
Dream Hunter <dreamhunter2333@gmail.com>
2025-03-16 14:20:24 +0800

    feat: support auto login with url query parameter (#606)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,18 +1,9 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
 import { createHead } from '@unhead/vue'
 
+import App from './App.vue'
+import router from './router'
 import i18n from './i18n'
-
-router.beforeEach((to, from) => {
-    if (to.params.lang && ['en', 'zh'].includes(to.params.lang)) {
-        i18n.global.locale.value = to.params.lang
-    } else {
-        i18n.global.locale.value = 'zh'
-    }
-});
-
 
 const head = createHead()
 const app = createApp(App)
