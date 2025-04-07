91a859bbcf2beb35f66e2c6951abc38387c5f143
Dream Hunter <dreamhunter2333@gmail.com>
2025-04-07 19:24:21 +0800

    feat: support cleanDays max 1000 (#622)

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -1,15 +1,19 @@
 cd frontend/
 pnpm up
+pnpm add -D wrangler@latest
 cd ..
 
 cd worker/
 pnpm up
+pnpm add -D wrangler@latest
 cd ..
 
 cd pages/
 pnpm up
+pnpm add -D wrangler@latest
 cd ..
 
 cd vitepress-docs/
 pnpm up
+pnpm add -D wrangler@latest
 cd ..
