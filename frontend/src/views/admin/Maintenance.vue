ad4dd83cc5fc766eba405ce560bf566fac8a6563
dreamhunter2333 <dreamhunter2333@gmail.com>
2025-06-18 14:02:46 +0800

    feat: cleanup support address and inactive address

--- frontend/src/views/admin/Maintenance.vue
+++ frontend/src/views/admin/Maintenance.vue
@@ -11,10 +11,12 @@ const cleanupModel = ref({
     cleanMailsDays: 30,
     enableUnknowMailsAutoCleanup: false,
     cleanUnknowMailsDays: 30,
-    enableAddressAutoCleanup: false,
-    cleanAddressDays: 30,
     enableSendBoxAutoCleanup: false,
     cleanSendBoxDays: 30,
+    enableAddressAutoCleanup: false,
+    cleanAddressDays: 30,
+    enableInactiveAddressAutoCleanup: false,
+    cleanInactiveAddressDays: 30,
 })
 
 const { t } = useI18n({
@@ -24,6 +26,8 @@ const { t } = useI18n({
             mailBoxLabel: 'Cleanup the inbox before n days',
             mailUnknowLabel: "Cleanup the unknow mail before n days",
             sendBoxLabel: "Cleanup the sendbox before n days",
+            addressCreateLabel: "Cleanup the address created before n days",
+            inactiveAddressLabel: "Cleanup the inactive address before n days",
             cleanupNow: "Cleanup now",
             autoCleanup: "Auto cleanup",
             cleanupSuccess: "Cleanup success",
@@ -35,6 +39,8 @@ const { t } = useI18n({
             mailBoxLabel: '清理 n 天前的收件箱',
             mailUnknowLabel: "清理 n 天前的无收件人邮件",
             sendBoxLabel: "清理 n 天前的发件箱",
+            addressCreateLabel: "清理 n 天前创建的地址",
+            inactiveAddressLabel: "清理 n 天前的未活跃地址",
             autoCleanup: "自动清理",
             cleanupSuccess: "清理成功",
             cleanupNow: "立即清理",
@@ -86,9 +92,14 @@ onMounted(async () => {
 <template>
     <div class="center">
         <n-card :bordered="false" embedded>
-            <n-alert :show-icon="false" :bordered="false">
+            <n-alert :show-icon="false" :bordered="false" type="warning">
                 <span>{{ t('cronTip') }}</span>
             </n-alert>
+            <n-flex justify="end">
+                <n-button @click="save" type="primary" :loading="loading">
+                    {{ t('save') }}
+                </n-button>
+            </n-flex>
             <n-form :model="cleanupModel">
                 <n-form-item-row :label="t('mailBoxLabel')">
                     <n-checkbox v-model:checked="cleanupModel.enableMailsAutoCleanup">
@@ -126,9 +137,30 @@ onMounted(async () => {
                         {{ t('cleanupNow') }}
                     </n-button>
                 </n-form-item-row>
-                <n-button @click="save" type="primary" block :loading="loading">
-                    {{ t('save') }}
-                </n-button>
+                <n-form-item-row :label="t('addressCreateLabel')">
+                    <n-checkbox v-model:checked="cleanupModel.enableAddressAutoCleanup">
+                        {{ t('autoCleanup') }}
+                    </n-checkbox>
+                    <n-input-number v-model:value="cleanupModel.cleanAddressDays" :placeholder="t('tip')" />
+                    <n-button @click="cleanup('addressCreated', cleanupModel.cleanAddressDays)">
+                        <template #icon>
+                            <n-icon :component="CleaningServicesFilled" />
+                        </template>
+                        {{ t('cleanupNow') }}
+                    </n-button>
+                </n-form-item-row>
+                <n-form-item-row :label="t('inactiveAddressLabel')">
+                    <n-checkbox v-model:checked="cleanupModel.enableInactiveAddressAutoCleanup">
+                        {{ t('autoCleanup') }}
+                    </n-checkbox>
+                    <n-input-number v-model:value="cleanupModel.cleanInactiveAddressDays" :placeholder="t('tip')" />
+                    <n-button @click="cleanup('inactiveAddress', cleanupModel.cleanInactiveAddressDays)">
+                        <template #icon>
+                            <n-icon :component="CleaningServicesFilled" />
+                        </template>
+                        {{ t('cleanupNow') }}
+                    </n-button>
+                </n-form-item-row>
             </n-form>
         </n-card>
     </div>
