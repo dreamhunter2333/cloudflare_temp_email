38ca311e3b742bb889e2967cf6238d9efe03c7c4
Hg <hging3@gmail.com>
2026-04-03 14:06:36 +0800

    fix: rabbit review

--- worker/src/mails_api/index.ts
+++ worker/src/mails_api/index.ts
@@ -53,10 +53,9 @@ api.delete('/api/mails/:id', async (c) => {
     }
     const { address } = c.get("jwtPayload")
     const { id } = c.req.param();
-    // TODO: add toLowerCase() to handle old data
     const { success } = await c.env.DB.prepare(
         `DELETE FROM raw_mails WHERE address = ? and id = ? `
-    ).bind(address.toLowerCase(), id).run();
+    ).bind(address, id).run();
     return c.json({
         success: success
     })
