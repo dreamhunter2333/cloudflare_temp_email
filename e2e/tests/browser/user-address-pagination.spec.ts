3af5a1ed31fd412796297c4addd47af658cab869
dreamhunter2333 <dreamhunter2333@gmail.com>
2026-09-23 20:16:20 +0800

    test: target address search input element

--- e2e/tests/browser/user-address-pagination.spec.ts
+++ e2e/tests/browser/user-address-pagination.spec.ts
@@ -95,7 +95,7 @@ test.describe('User address pagination browser flow', () => {
 
       const selectedAddress = createdAddresses[20];
 
-      const addressSearch = page.getByTestId('user-address-search');
+      const addressSearch = page.getByTestId('user-address-search').locator('input');
       await addressSearch.fill(selectedAddress.address);
       await addressSearch.press('Enter');
       await expect(pagination).toContainText(/Total:\s*1/);
